c0b9328dab2b58501cf40ba3c407adcb0c1ae970
TianRy1337 <Ryan55014TianZhang@gmail.com>
2021-02-08 22:53:51 +0800

    2021/02/08
    補齊遺失的進度

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<WayPoint2> path;
+
+    void Start()
+    {
+        StartCoroutine(watToPath());
+    }
+    IEnumerator watToPath()
+    {
+        print("Strat Patrolling");
+        foreach (WayPoint2 waypoint in path)
+        {
+            transform.position = waypoint.transform.position; //new Vector3(waypoint.transform.position.x, waypoint.transform.position.y + 10f, waypoint.transform.position.z);
+            print("Visiting Block :" +waypoint.name);
+            yield return new WaitForSecondsRealtime(1);
+        }
+        print("End Patrol");
+        
+
+    }
+
+    void Update()
+    {
+
+    }
+}
