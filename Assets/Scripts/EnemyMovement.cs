8f68d401321176c652d2cac90d73e55dff30344f
TianRy1337 <Ryan55014TianZhang@gmail.com>
2021-02-27 00:21:02 +0800

    2021/02/26
    -新增敵人到達目的地特效

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     //[SerializeField] List<WayPoint2> path;
-
+    [SerializeField] float movementPeriod = .5f;
+    [SerializeField] ParticleSystem goalParticle;
     void Start()
     {
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
@@ -21,9 +22,17 @@ public class EnemyMovement : MonoBehaviour
         {
             transform.position = waypoint.transform.position; //new Vector3(waypoint.transform.position.x, waypoint.transform.position.y + 10f, waypoint.transform.position.z);
             //print("Visiting Block :" +waypoint.name);
-            yield return new WaitForSecondsRealtime(3);
+            yield return new WaitForSecondsRealtime(movementPeriod);
         }
+        SelfDestruct();
     }
 
-
+    void SelfDestruct()
+    {
+        var vfx = Instantiate(goalParticle,transform.position,Quaternion.identity);
+        vfx.Play();
+        float destroyDelay = vfx.main.duration;
+        Destroy(vfx.gameObject,destroyDelay);
+        Destroy(gameObject);
+    }
 }
